ad88cb0e05c7f8e61f168167306d4e89a7cc372c
MAZE <remvze@proton.me>
2025-07-21 18:45:18 +0330

    chore: rename and remove extra packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: 'https://mnmm.xyz',
+  site: 'https://usecn.mvze.net',
 });
